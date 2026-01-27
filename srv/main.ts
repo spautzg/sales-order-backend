4265d7bb713ed902c49831fb886756dc254c31f9
Gilvan Spautz <gilvan.spautz@outlook.com>
2026-01-26 23:57:42 -0300

    feat: add new feature in tsconfig.json

--- srv/main.ts
+++ srv/main.ts
@@ -1,2 +1,13 @@
+import { Customer, Customers} from '@models/sales'
+
+const customer: Customer = {
+    email: 'teste@teste.com',
+    firstName: 'Gilvan',
+    lastName: 'Spautz',
+    id: '1234'
+};
+
+const customers: Customers = [customer]
+
 const funcao = (variavel: string) => console.log(variavel);
 funcao('123');
