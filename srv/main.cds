f093c42bac0554f764539843c8933aae3009c6ee
Gilvan Spautz <gilvan.spautz@outlook.com>
2026-01-27 22:12:01 -0300

    feat: add new logic validation

--- srv/main.cds
+++ srv/main.cds
@@ -2,7 +2,7 @@ using { sales } from '../db/schema';
 
 service MainService {
 
-    entity SalesOrderHearder as projection on sales.SalesOrderHeaders;
+    entity SalesOrderHeaders as projection on sales.SalesOrderHeaders;
     entity Customers as projection on sales.Customers;
     entity Products as projection on sales.Products;
 
--- srv/main.ts
+++ srv/main.ts
@@ -1,4 +1,4 @@
-import { Service } from '@sap/cds';
+import cds, { Request, Service } from '@sap/cds';
 import { Customers } from '@models/sales';
 
 export default (service: Service) => {
@@ -8,5 +8,19 @@ export default (service: Service) => {
                 customer.email = `${customer.email}@gmail.com`;
             }
         })
-    })
+    });
+    service.before('CREATE', 'SalesOrderHeaders', async (request: Request) => {
+        const params = request.data;
+        if (!params.customer_id) {
+        return request.reject(400, 'Invalid Customer');
+        }
+        if (!params.items || params.items?.length === 0) {
+            return request.reject(400, 'invalid items');
+        }
+        const customerQuery = SELECT.one.from('sales.Customers').where({ id: params.customer_id });
+        const customer = await cds.run(customerQuery);
+        if (!customer) {
+            return request.reject(404, 'customer not found');
+        }
+});
 }
