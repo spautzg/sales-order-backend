ab6acb0c950e81de39573fc96c0b8abece7ebb8d
Gilvan Spautz <gilvan.spautz@outlook.com>
2026-02-02 17:14:35 -0300

    feat: baixa estoque ao criar ordem de venda

--- srv/main.ts
+++ srv/main.ts
@@ -1,5 +1,5 @@
 import cds, { Request, Service } from '@sap/cds';
-import { Customers, Product, Products, SalesOrderHeader, SalesOrderItem, SalesOrderItems } from '@models/sales';
+import { Customers, Product, Products, SalesOrderHeader, SalesOrderHeaders, SalesOrderItem, SalesOrderItems } from '@models/sales';
 
 export default (service: Service) => {
     service.after('READ', 'Customers', (results: Customers) => {
@@ -37,4 +37,22 @@ export default (service: Service) => {
 
         }
     });
+    service.after('CREATE', 'SalesOrderHeaders', async (results: SalesOrderHeaders) => {
+        const headersAsArray = Array.isArray(results) ? results : [results] as SalesOrderHeaders;
+        for (const header of headersAsArray) {
+            const items = header.items as SalesOrderItems;
+            const productsData = items.map(item => ({
+                id: item.product_id as string,
+                quantity: item.quantity as number
+            }));
+            const productsIds: string[] = productsData.map((productData) => productData.id);
+            const productsQuery = SELECT.from('sales.Products').where({ id: productsIds });
+            const products: Products = await cds.run(productsQuery);
+            for (const productData of productsData) {
+                const foundProduct = products.find(product => product.id === productData.id) as Product;
+                foundProduct.stock = (foundProduct.stock as number) - productData.quantity;
+                await cds.update('sales.Products').where({ id:foundProduct.id }).with({ stock: foundProduct.stock });
+            }
+            }
+    })
 }
