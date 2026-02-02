702098009ce9d24456a57e03195068755befc435
Gilvan Spautz <gilvan.spautz@outlook.com>
2026-02-02 16:23:50 -0300

    feat: add products validation

--- srv/main.ts
+++ srv/main.ts
@@ -1,8 +1,8 @@
 import cds, { Request, Service } from '@sap/cds';
-import { Customers } from '@models/sales';
+import { Customers, Product, Products, SalesOrderHeader, SalesOrderItem, SalesOrderItems } from '@models/sales';
 
 export default (service: Service) => {
-    service.after( 'READ', 'Customers', (results: Customers) => {
+    service.after('READ', 'Customers', (results: Customers) => {
         results.forEach(customer => {
             if (!customer.email?.includes('@')) {
                 customer.email = `${customer.email}@gmail.com`;
@@ -11,8 +11,9 @@ export default (service: Service) => {
     });
     service.before('CREATE', 'SalesOrderHeaders', async (request: Request) => {
         const params = request.data;
+        const items: SalesOrderItems= params.items;
         if (!params.customer_id) {
-        return request.reject(400, 'Invalid Customer');
+            return request.reject(400, 'Invalid Customer');
         }
         if (!params.items || params.items?.length === 0) {
             return request.reject(400, 'invalid items');
@@ -22,5 +23,18 @@ export default (service: Service) => {
         if (!customer) {
             return request.reject(404, 'customer not found');
         }
-});
+        const productsIds: string[] = params.items.map((item: SalesOrderItem) => item.product_id);
+        const productsQuery = SELECT.from('sales.products').where({ id: productsIds });
+        const products: Products = await cds.run(productsQuery);
+        for (const item of items) {
+            const dbProduct = products.find(product => product.id === item.product_id);
+            if (!dbProduct) {
+                return request.reject(404, `Produto ${item.product_id} não encontrado`);
+            }
+            if (dbProduct.stock === 0) {
+                return request.reject(400, `Produto ${dbProduct.name}(${dbProduct.id}) sem estoque disponível`);
+            }
+
+        }
+    });
 }
