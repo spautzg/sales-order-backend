6f4e71b88d894258ab02fec3e457ac5cdead2ecf
Gilvan Spautz <gilvan.spautz@outlook.com>
2026-02-16 20:33:59 -0300

    refactor: change name method call

--- srv/services/customer/implementation.ts
+++ srv/services/customer/implementation.ts
@@ -5,7 +5,7 @@ import { CustomerService } from "./protocols";
 export class CustomerServiceImpl implements CustomerService {
     public afterRead(customerList: Customers): Customers {
         const customers = customerList.map(c => {
-            const customer = CustomerModel.create({
+            const customer = CustomerModel.with({
                 id: c.id as string,
                 firstName: c.firstName as string,
                 lastName: c.lastName as string,
